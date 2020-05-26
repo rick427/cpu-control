3d55d58dec66c33d5ed577e7e3848466e129ae12
rick427 <rchard@yahoo.com>
2020-05-26 14:20:30 +0100

    interval and cpu overload

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,6 +4,32 @@ const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
+let cpuOverload = 80;
+
+// Run every 2 seconds
+setInterval(() => {
+    // cpu usage
+    cpu.usage().then(info => {
+        document.getElementById('cpu-usage').innerText = info + '%';
+        document.getElementById("cpu-progress").style.width = info + '%';
+
+        if(info >= cpuOverload){
+            document.getElementById("cpu-progress").style.background = 'red';
+        }
+        else{
+            document.getElementById("cpu-progress").style.background = '#30c88b';
+        }
+    });
+
+    // cpu free
+    cpu.free().then(info => {
+        document.getElementById('cpu-free').innerText = info + '%'
+    });
+
+    // uptime
+    document.getElementById('sys-uptime').innerText = secondsToDhms(os.uptime());
+},2000)
+
 // set model
 document.getElementById('cpu-model').innerText = cpu.model();
 
@@ -16,4 +42,15 @@ document.getElementById('os').innerText = `${os.type()} ${os.arch()}`
 // total memory
 mem.info().then(info => {
     document.getElementById('mem-total').innerText = info.totalMemMb
-})
+});
+
+//show days, hours, mins, secs
+function secondsToDhms(seconds){
+    seconds = +seconds;
+    const day = Math.floor(seconds / (3600 * 24));
+    const hour = Math.floor((seconds % (3600 * 24)) / 3600);
+    const mins = Math.floor((seconds % 3600) / 60);
+    const sec = Math.floor(seconds % 60);
+
+    return `${day}d, ${hour}h, ${mins}m, ${sec}s`;
+}
