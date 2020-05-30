f77e783ae3a6e7396523775e6254d6b1d5181a2f
rick427 <rchard@yahoo.com>
2020-05-30 10:14:03 +0100

    toggle navigation func

--- main.js
+++ main.js
@@ -95,6 +95,15 @@ const menu = [
   {
     role: 'fileMenu',
   },
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Toggle Navigation',
+        click: () => mainWindow.webContents.send('nav:toggle')
+      }
+    ]
+  },
   ...(isDev
     ? [
         {
